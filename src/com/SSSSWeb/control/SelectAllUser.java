c179ab3096c1aa697bef721687f7e34291f468ed
shuye <shuye00123@qq.com>
2016-03-14 19:34:53 +0800

    补充

---
+++ src/com/SSSSWeb/control/SelectAllUser.java
@@ -0,0 +1,41 @@
+package com.SSSSWeb.control;
+
+import java.util.List;
+
+import com.SSSSWeb.model.business.service.UserService;
+import com.SSSSWeb.model.domain.User;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class SelectAllUser extends ActionSupport implements ModelDriven<User>{
+	private List list;
+	private User user = new User();
+	private UserService service;
+	public List getList() {
+		return list;
+	}
+	public void setList(List list) {
+		this.list = list;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public UserService getService() {
+		return service;
+	}
+	public void setService(UserService service) {
+		this.service = service;
+	}
+	@Override
+	public String execute() throws Exception {
+		list = service.SelectAllUser();
+		return "success";
+	}
+	public User getModel() {
+		// TODO Auto-generated method stub
+		return user;
+	}
+}
